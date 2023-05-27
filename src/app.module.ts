b7b58485e8a371b5a7ca79541b4945bb57f9217c
este <djjd2000@naver.com>
2023-05-27 23:12:31 +0900

    Chore: initialized controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController } from './posts/posts.controller';
+import { CategoriesController } from './categories/categories.controller';
+import { CommentsController } from './comments/comments.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PostsController, CategoriesController, CommentsController],
   providers: [AppService],
 })
 export class AppModule { }
