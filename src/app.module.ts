19f6385d2f6a9cc6ac01ef4e389d07b270111223
este <djjd2000@naver.com>
2023-05-28 23:53:32 +0900

    Chore: initialized env vars and orm options

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
 import { CategoriesController } from './categories/categories.controller';
 import { CommentsController } from './comments/comments.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      username: process.env.MYSQL_ID,
+      password: process.env.MYSQL_PW,
+      database: process.env.MYSQL_DB,
+      port: parseInt(String(process.env.MYSQL_PORT)),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController, PostsController, CategoriesController, CommentsController],
   providers: [AppService],
 })
