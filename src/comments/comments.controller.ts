88f8092730c876ec0a568578f329541c20f7dad6
este <djjd2000@naver.com>
2023-05-30 23:36:06 +0900

    Feature: added mock api and DTOs of comments

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
-@Controller('comments')
-export class CommentsController { }
+@Controller('posts/:post_uuid/comments')
+export class CommentsController {
+  @Get()
+  @HttpCode(200)
+  getCommentsByPostUuid(
+    @Param('post_uuid') postUuid: string,
+  ) {
+    return;
+  }
+
+  @Post()
+  @HttpCode(201)
+  createComment(
+    @Param('post_uuid') postUuid: string,
+    @Body() createCommentDto: CreateCommentDto,
+  ) {
+    return;
+  }
+
+  @Patch(':comment_uuid')
+  @HttpCode(200)
+  updateCommentByUuid(
+    @Param('post_uuid') postUuid: string,
+    @Param('comment_uuid') commentUuid: string,
+    @Body() updateCommentDto: UpdateCommentDto,
+  ) {
+    return;
+  }
+
+  @Delete(':comment_uuid')
+  @HttpCode(204)
+  deleteCommentByUuid(
+    @Param('post_uuid') postUuid: string,
+    @Param('comment_uuid') commentUuid: string,
+  ) {
+    return;
+  }
+}
---
+++ src/comments/dto/get-comment.ts
@@ -0,0 +1,11 @@
+export class CommentDTO {
+  readonly uuid: string;
+
+  readonly postUuid: string;
+
+  readonly userId: number;
+
+  readonly content: string;
+
+  readonly createdAt: string;
+}
