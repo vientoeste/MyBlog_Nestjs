98179cad445c000a1ddf1eb3969bd6d471bc50d8
este <djjd2000@naver.com>
2023-05-31 22:08:47 +0900

    Chore: edited .env and eslint config

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      envFilePath: [`.${process.env.NODE_ENV}.env`],
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
