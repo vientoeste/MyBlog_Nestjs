60c751de8d207799ebd969ac37dae81b8e365af6
este <djjd2000@naver.com>
2023-07-04 21:34:07 +0900

    Fix: added missing dependency of Post Repository for category

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,11 +3,13 @@ import { CategoriesService } from './categories.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryEntity } from './entities/category.entity';
 import { CategoryHistoryEntity } from './entities/category_history.entity';
+import { PostEntity } from 'src/posts/entities/post.entity';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([CategoryEntity]),
         TypeOrmModule.forFeature([CategoryHistoryEntity]),
+        TypeOrmModule.forFeature([PostEntity]),
     ],
     providers: [CategoriesService],
 })
